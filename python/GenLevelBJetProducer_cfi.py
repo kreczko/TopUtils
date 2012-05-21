5460e763d6c50ffea5535d63675b77d338cfae2e
cdiez <cdiez>
2012-05-21 15:02:58 +0000

    Replaced HLT for SIM in genJets

--- python/GenLevelBJetProducer_cfi.py
+++ python/GenLevelBJetProducer_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 produceGenLevelBJets = cms.EDProducer("GenLevelBJetProducer",
     ttGenEvent = cms.InputTag('genEvt'),
-    genJets = cms.InputTag('ak5GenJets','','HLT'),
+    genJets = cms.InputTag('ak5GenJets','','SIM'), 
     deltaR = cms.double(0.5),
     resolveParticleName = cms.bool(False),
     requireTopBquark = cms.bool(False),
--- python/genlevelbjetproducer_cfi.py
+++ python/genlevelbjetproducer_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 produceGenLevelBJets = cms.EDProducer('GenLevelBJetProducer',
   ttGenEvent = cms.InputTag('genEvt'),
-  genJets = cms.InputTag('ak5GenJets', '', 'HLT'),
+  genJets = cms.InputTag('ak5GenJets', '', 'SIM'), #REPLACED HLT FOR SIM
   deltaR = cms.double(5.0),
   noBBbarResonances = cms.bool(True)
 )
