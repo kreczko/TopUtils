0dc2b907b09079e07d7bb2deb974d532899f5c06
nbartosi <nbartosi>
2013-03-15 14:43:44 +0000

    Fixed name of input genJet collection to one with injected hadrons

--- python/GenLevelBJetProducer_cfi.py
+++ python/GenLevelBJetProducer_cfi.py
@@ -3,13 +3,13 @@ import FWCore.ParameterSet.Config as cms
 produceGenLevelBJets = cms.EDProducer("GenLevelBJetProducer",
     ttGenEvent = cms.InputTag('genEvt'),
 #    genJets = cms.InputTag('ak5GenJets','','SIM'),
-    genJets = cms.InputTag('ak5GenJets','',''),
+#    genJets = cms.InputTag('ak5GenJets','',''),
+    genJets = cms.InputTag('ak5GenJetsPlusHadron','',''),
     deltaR = cms.double(0.5),
     flavour = cms.int32(5),
     resolveParticleName = cms.bool(False),
     requireTopBquark = cms.bool(False),
     noBBbarResonances = cms.bool(True),
-    saveAllMothers = cms.bool(False),
     doImprovedHadronMatching = cms.bool(False),
     doValidationPlotsForImprovedHadronMatching = cms.bool(False)
 )
